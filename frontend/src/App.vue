62c88257b610c7dea78ad4088e6fa09fc80dc38e
ppoffice <zrp1994@gmail.com>
2018-02-22 23:06:18 -0600

    chore(frontend): move user consent dialog to root component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,24 @@
         <router-view></router-view>
       </div>
     </section>
+    <div v-if="this.$store.getters.authenticated && !this.$store.state.userConsent" class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box">
+          <h3 class="title is-3">User Agreement</h3>
+          <p>TODO: add text here</p>
+          <div class="field">
+            <label class="label">Your name:</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Enter your name here to accept these terms above" v-model="consentUserName">
+            </div>
+          </div>
+          <button v-if="consentUserName" class="button" @click="()=>this.$store.commit('setUserConsent', true)">Accept</button>
+          <button v-else disabled class="button">Accept</button>
+          <button class="button" @click="()=>{this.$store.commit('logout'); this.$router.push('/login');}">Decline</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,6 +37,11 @@
     name: 'app',
     components: {
       AppNav: AppNav
+    },
+    data() {
+      return {
+        consentUserName: null
+      }
     },
     beforeCreate() {
       if (this.$store.getters.authenticated) {
