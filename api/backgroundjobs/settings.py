7ebd80b93dbcc2c86326ee507249331dd4e0a953
Dung Viet Nguyen <dvnguyen.vn@gmail.com>
2018-04-02 00:20:08 -0500

    Set SENTRY_DSN to be optional in worker

--- api/backgroundjobs/settings.py
+++ api/backgroundjobs/settings.py
@@ -7,4 +7,5 @@ REDIS_HOST = os.environ['REDIS_HOST']
 REDIS_PORT = os.environ['REDIS_PORT']
 REDIS_DB = os.environ['REDIS_JOBS_DB']
 
-SENTRY_DSN = 'sync+' + os.environ['SENTRY_DSN']
+if os.environ.get('SENTRY_DSN') is not None:
+    SENTRY_DSN = 'sync+' + os.environ['SENTRY_DSN']
